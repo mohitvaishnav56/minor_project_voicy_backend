3e5b560d0c5c20b54041bb42559e4b7abec58c8c
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2025-12-05 15:23:36 +0530

    logout functionality and login functionality added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import User from "../models/user.model.js"
-import {ApiError} from "../utils/ApiResponse.js"
+import {User} from "../models/user.model.js"
+import {ApiError} from "../utils/ApiError.js"
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
 
