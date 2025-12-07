e2f2b73510afb6f970afcfb5a5264d54ba5aa265
Roshanbadgujar <hacktecher04@gmail.com>
2025-12-07 18:19:54 +0530

    update imagekit work

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
+import "dotenv/config.js";
 import connectDB from "./src/db/index.js";
 import app from "./src/app.js";
 
-dotenv.config();
-connectDB()
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
